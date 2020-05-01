b5bef0f2c5e5eb5ebd0043bfc1242b906588f418
harsh99sandhu <64521805+harsh99sandhu@users.noreply.github.com>
2020-05-01 17:23:36 +0530

    Basic corrections
    Corrected Class name and comments

--- 01Day1/HELLOWORLD/helloworld.java
+++ 01Day1/HELLOWORLD/helloworld.java
@@ -1,9 +1,14 @@
 package DAY1;
 
-public class helloworld {
-	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");// here System.out.println is a out put statement like cout or printf in cpp
-											// and c respectively
+public class Hello_World
+//It is a good practice to start the name of the Class with a capital letter
+{
+	public static void main(String[] args) 
+	//This line acts as the entry point of the programme 	
+	{
+		System.out.println("HELLO WORLD");
+		// here System.out.println is used to print the argument present between the quotation mark
+		//like cout or printf in cpp and c respectively
 	}
 
 }
