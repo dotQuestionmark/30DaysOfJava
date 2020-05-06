2f0e8bf152444df88741236ffa4afa0b639958d4
Pranit5895 <guptapranit9@gmail.com>
2020-05-06 03:05:24 -0700

    Converted to write mode when password match

--- 06Day6/Task/Solution-Q1.java
+++ 06Day6/Task/Solution-Q1.java
@@ -66,4 +66,41 @@ public class Files {
 				System.out.println("Error in InputStream close(): " + ioe);
 			}
 		}
+	str Pass2= in.nextInt();
+	if(pass==Pass2)
+	{
+		try {
+			String mycontent = in.nextLine();
+			// Specify the file name and path here
+			File file = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt");
+
+			/*
+			 * This logic will make sure that the file gets created if it is not present at
+			 * the specified location
+			 */
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			bw.write(mycontent);
+			System.out.println("File written Successfully");
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+			} catch (Exception ex) {
+				System.out.println("Error in closing the BufferedWriter" + ex);
+			}
+		}
+	}
+	else
+	{ System.out.println("Wrong Password");
+	}
+    }
+}
 
