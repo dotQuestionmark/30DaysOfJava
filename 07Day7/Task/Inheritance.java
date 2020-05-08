c74f6d61925a124fd497c2c0f01529c082934c02
Pranit5895 <guptapranit9@gmail.com>
2020-05-07 21:41:23 -0700

    Implementaton of java

---
+++ 07Day7/Task/Inheritance.java
@@ -0,0 +1,16 @@
+//Java program to illustrate the  
+// concept of inheritance 
+  
+// base class 
+class Bicycle  
+{ 
+    // the Bicycle class has two fields 
+    public int gear; 
+    public int speed; 
+          
+    // the Bicycle class has one constructor 
+    public Bicycle(int gear, int speed) 
+    { 
+        this.gear = gear; 
+        this.speed = speed; 
+    } 
