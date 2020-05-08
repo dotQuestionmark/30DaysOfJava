855978ddd1303f55930d67317b210c087b3afc34
Pranit5895 <guptapranit9@gmail.com>
2020-05-07 21:43:14 -0700

    Class mountainbike is derived class hence code completed

--- 07Day7/Task/Inheritance.java
+++ 07Day7/Task/Inheritance.java
@@ -33,3 +33,47 @@ class Bicycle
                 + "speed of bicycle is "+speed);
     }
 }
+// derived class
+class MountainBike extends Bicycle
+{
+
+    // the MountainBike subclass adds one more field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int gear,int speed,
+                        int startHeight)
+    {
+        // invoking base-class(Bicycle) constructor
+        super(gear, speed);
+        seatHeight = startHeight;
+    }
+
+    // the MountainBike subclass adds one more method
+    public void setHeight(int newValue)
+    {
+        seatHeight = newValue;
+    }
+
+    // overriding toString() method
+    // of Bicycle to print more info
+    @Override
+    public String toString()
+    {
+        return (super.toString()+
+                "\nseat height is "+seatHeight);
+    }
+
+}
+
+// driver class
+public class Test
+{
+    public static void main(String args[])
+    {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+
+    }
+}
