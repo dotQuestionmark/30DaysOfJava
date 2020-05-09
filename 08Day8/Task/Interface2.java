0f1199b5e6b55f7f5d94d1879f19ce9cb30e5f6a
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 01:02:50 -0700

    class bicycle implementing interface vehicle

--- 08Day8/Task/Interface2.java
+++ 08Day8/Task/Interface2.java
@@ -7,3 +7,21 @@ interface Vehicle {
     void speedUp(int a); 
     void applyBrakes(int a); 
 } 
+class Bicycle implements Vehicle{
+
+    int speed;
+    int gear;
+
+     // to change gear
+    @Override
+    public void changeGear(int newGear){
+
+        gear = newGear;
+    }
+
+    // to increase speed
+    @Override
+    public void speedUp(int increment){
+
+        speed = speed + increment;
+    }
