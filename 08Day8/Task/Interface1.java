c65c8a761cdd777420080ac399a8ecf3e8f50e07
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 01:00:19 -0700

    Implementation of interface from class

--- 08Day8/Task/Interface1.java
+++ 08Day8/Task/Interface1.java
@@ -10,4 +10,22 @@ interface In1
   
     // public and abstract  
     void display(); 
-} 
+}
+// A class that implements the interface.
+class TestClass implements In1
+{
+    // Implementing the capabilities of
+    // interface.
+    public void display()
+    {
+        System.out.println("Geek");
+    }
+
+    // Driver Code
+    public static void main (String[] args)
+    {
+        TestClass t = new TestClass();
+        t.display();
+        System.out.println(a);
+    }
+}
