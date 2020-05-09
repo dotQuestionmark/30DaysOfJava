e67b9f4b8b27fd909bb5c3213bef445c5d56af86
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 01:06:10 -0700

    class declared for function calling and interface implementation

--- 08Day8/Task/Interface2.java
+++ 08Day8/Task/Interface2.java
@@ -67,4 +67,29 @@ class Bike implements Vehicle {
              + " gear: " + gear); 
     } 
       
-}  
+}
+class GFG {
+
+    public static void main (String[] args) {
+
+        // creating an inatance of Bicycle
+        // doing some operations
+        Bicycle bicycle = new Bicycle();
+        bicycle.changeGear(2);
+        bicycle.speedUp(3);
+        bicycle.applyBrakes(1);
+
+        System.out.println("Bicycle present state :");
+        bicycle.printStates();
+
+        // creating instance of the bike.
+        Bike bike = new Bike();
+        bike.changeGear(1);
+        bike.speedUp(4);
+        bike.applyBrakes(3);
+
+        System.out.println("Bike present state :");
+        bike.printStates();
+    }
+}
+
