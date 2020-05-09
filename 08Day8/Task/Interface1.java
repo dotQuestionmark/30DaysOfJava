7092454799e01ecd8f8bb3296a399801147291f5
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 00:59:28 -0700

    Basic function of interface declared

---
+++ 08Day8/Task/Interface1.java
@@ -0,0 +1,13 @@
+// Java program to demonstrate working of  
+// interface. 
+import java.io.*; 
+  
+// A simple interface 
+interface In1 
+{ 
+    // public, static and final 
+    final int a = 10; 
+  
+    // public and abstract  
+    void display(); 
+} 
