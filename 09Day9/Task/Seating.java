50ab6f34f8c3e78308b6bdf554882e9967395319
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 03:47:19 -0700

    Checking Seat type

--- 09Day9/Task/Seating.java
+++ 09Day9/Task/Seating.java
@@ -11,3 +11,13 @@ public class seatingarr {
 			int seat = in.nextInt();
 			// calculating the facing seat number
 			seat = seat + 2 * (6 - (seat - 1) % 12) - 1;
+			// checking the seat type
+			if (seat % 6 < 2)
+				System.out.println(seat + " " + "WS");
+			else if (seat % 6 == 2 || seat % 6 == 5)
+				System.out.println(seat + " " + "MS");
+			else
+				System.out.println(seat + " " + "AS");
+		}
+	}
+}
