9962c95ef1dd98f0236ec573ac00c8b43c92ae52
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 03:45:33 -0700

    Facing seat number

--- 09Day9/Task/Seating.java
+++ 09Day9/Task/Seating.java
@@ -5,3 +5,9 @@ public class seatingarr {
 		Scanner in = new Scanner(System.in);
 		// ask for number of inputs
 		int x = in.nextInt();
+		// for loop
+		for (int i = 0; i < x; i++) {
+			// input the number of seat
+			int seat = in.nextInt();
+			// calculating the facing seat number
+			seat = seat + 2 * (6 - (seat - 1) % 12) - 1;
