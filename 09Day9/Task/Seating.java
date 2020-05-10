cf1e9596af130e56fccfe9d06a0141400cb70e09
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 03:43:08 -0700

    Input from user for seat number

---
+++ 09Day9/Task/Seating.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class seatingarr {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// ask for number of inputs
+		int x = in.nextInt();
