b9db826eb83d4bb04798bb6795681bf64cd04741
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:10:54 -0700

    Comments added for explaination

--- 10Day10/Task/Q1.java
+++ 10Day10/Task/Q1.java
@@ -1,19 +1,19 @@
-class Rectangle{
+class Rectangle{         //OVERLOADING CLASS
 
   public static void printArea(int x,int y){
 	  System.out.println(x*y);
 	   }
-  public static void printArea(int x){
+  public static void printArea(int x){       //overloading function 1
     System.out.println(x*x);
   }
-  public static void printArea(int x,double y){
+  public static void printArea(int x,double y){  //overloading function 2
     System.out.println(x*y);
   }
-  public static void printArea(double x){
+  public static void printArea(double x){   //overloading function 3
     System.out.println(x*x);
     }
 
-  public static void main(String[] args){
+  public static void main(String[] args){   //main fucntion for implementation
     printArea(2,4);
     printArea(2,5.1);
     printArea(10);
