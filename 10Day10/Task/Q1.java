325c8ba54133c17150341bfb3d6852b37ab4c8ce
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:15:57 -0700

    Commenst added and implementation done

--- 10Day10/Task/Q1.java
+++ 10Day10/Task/Q1.java
@@ -21,27 +21,27 @@ class Rectangle{         //OVERLOADING CLASS
 }
 }
 
-class Animals{
+class Animals{        //CLASS FOR OVERRIDING
   public void sound(){
     System.out.println("This is parent class.");
   }
 }
-class Dogs extends Animals{
+class Dogs extends Animals{    //inherited class 1
   public void sound(){
     System.out.println("Dogs bark");
   }
 }
-class Cats extends Animals{
+class Cats extends Animals{    //inherited class 2
   public void sound(){
     System.out.println("Cats meow");
   }
 }
-class Monkeys extends Animals{
+class Monkeys extends Animals{    //inherited class 3
   public void sound(){
     System.out.println("Monkeys whoop.");
   }
 }
-class m{
+class m{       //driver class with main functin
 public static void main(String[] args){
   Animals d = new Dogs();
   Animals c = new Cats();
