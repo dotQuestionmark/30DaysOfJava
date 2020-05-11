950806dc058700085e266bc856e465637a837e49
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:08:50 -0700

    Functions for overloading

--- 10Day10/Task/Q1.java
+++ 10Day10/Task/Q1.java
@@ -1,4 +1,13 @@
 class Rectangle{
 
   public static void printArea(int x,int y){
+	  System.out.println(x*y);
+	   }
+  public static void printArea(int x){
+    System.out.println(x*x);
+  }
+  public static void printArea(int x,double y){
     System.out.println(x*y);
+  }
+  public static void printArea(double x){
+    System.out.println(x*x);
