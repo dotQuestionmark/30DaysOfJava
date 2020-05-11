9811fab4bc483b49c4ddbfcc5b81919d59623130
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:09:25 -0700

    Class rectangle completed hence overloading implementation

--- 10Day10/Task/Q1.java
+++ 10Day10/Task/Q1.java
@@ -11,3 +11,12 @@ class Rectangle{
   }
   public static void printArea(double x){
     System.out.println(x*x);
+    }
+
+  public static void main(String[] args){
+    printArea(2,4);
+    printArea(2,5.1);
+    printArea(10);
+    printArea(2.3);
+}
+}
