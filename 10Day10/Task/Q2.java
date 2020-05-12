e5010cb2ffbb0aed154e3000ee6e5ea314b4b1a8
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 03:43:31 -0700

    1st way

--- 10Day10/Task/Q2.java
+++ 10Day10/Task/Q2.java
@@ -1,3 +1,7 @@
 import java.util.regex.*;  
 public class RegexExample1{  
-public static void main(String args[]){  
+public static void main(String args[]){ 
+	//1st way
+Pattern p = Pattern.compile(".s");//. represents single character
+Matcher m = p.matcher("as");
+boolean b = m.matches();
