343d281e8fff0e9318cb3dfcaaf928fa107a3f85
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:17:52 -0700

    Way 1 to convert file to read mode

--- 12Day12/SOLUTION/Solution2.java
+++ 12Day12/SOLUTION/Solution2.java
@@ -64,3 +64,13 @@ public class Files {
 		}
 		BufferedReader br = null;
 		BufferedReader br2 = null;
+		try {
+			br = new BufferedReader(new FileReader("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt"));
+
+			// One way of reading the file
+			System.out.println("Reading the file using readLine() method:");
+			String contentLine = br.readLine();
+			while (contentLine != null) {
+				System.out.println(contentLine);
+				contentLine = br.readLine();
+			}
