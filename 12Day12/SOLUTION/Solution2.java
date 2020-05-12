9d77a1b598262038c8315c0dca387988fd9ef07d
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:11:19 -0700

    Class file started and input from user done

--- 12Day12/SOLUTION/Solution2.java
+++ 12Day12/SOLUTION/Solution2.java
@@ -6,3 +6,13 @@ import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.Scanner;
 
+public class Files {
+
+    public static void main(String args[]){
+
+        System.out.print("Enter no: ");
+	Scanner scan = new Scanner(System.in);
+	int one = scan.newInt();
+	int two = scan.newInt();         //input no from user
+        FileWriter fWriter = null;
+        BufferedWriter writer = null;
