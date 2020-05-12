8f6e1041e3458f0c54e3c48ffc6ad2e05d605fa4
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:14:21 -0700

    Try block for saving input into files

--- 12Day12/SOLUTION/Solution2.java
+++ 12Day12/SOLUTION/Solution2.java
@@ -31,3 +31,20 @@ public class Files {
 		File file = new File("D:\\JAVA\\newfile.txt");
 		BufferedInputStream bis = null;
 		FileInputStream fis = null;
+		try {
+			// FileInputStream to read the file
+			fis = new FileInputStream(file);
+
+			/*
+			 * Passed the FileInputStream to BufferedInputStream For Fast read using the
+			 * buffer array.
+			 */
+			bis = new BufferedInputStream(fis);
+
+			/*
+			 * available() method of BufferedInputStream returns 0 when there are no more
+			 * bytes present in the file to be read
+			 */
+			while (bis.available() > 0) {
+				System.out.print((char) bis.read());
+			}
