e6f0e307e235594e68e7b41f33b3503455723351
Pranit5895 <guptapranit9@gmail.com>
2020-05-13 10:52:31 -0700

    Method two for overriding

--- 12Day12/SOLUTION/Solution4.java
+++ 12Day12/SOLUTION/Solution4.java
@@ -13,3 +13,27 @@ class MyChildClass extends MyBaseClass{
       obj.disp();
    }
 }
+
+class ABC{
+   public void myMethod()
+   {
+	System.out.println("Overridden method");
+   }
+}
+class Demo extends ABC{
+   public void myMethod(){
+	//This will call the myMethod() of parent class
+	super.myMethod();
+	System.out.println("Overriding method");
+   }
+   public static void main( String args[]) {
+	Demo obj = new Demo();
+	obj.myMethod();
+   }
+}class ABC{
+   public void myMethod()
+   {
+	System.out.println("Overridden method");
+   }
+}
+
