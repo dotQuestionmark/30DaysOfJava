1bace6cfbccdee62db7d5d7543ae783f27e890ad
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 00:46:34 -0700

    Functions of stack implementation started

--- 12Day12/SOLUTION/Solution7.java
+++ 12Day12/SOLUTION/Solution7.java
@@ -5,3 +5,34 @@ class Stack
 	private int arr[];
 	private int top;
 	private int capacity;
+
+	// Constructor to initialize stack
+	Stack(int size)
+	{
+		arr = new int[size];
+		capacity = size;
+		top = -1;
+	}
+
+	// Utility function to add an element x in the stack
+	public void push(int x)
+	{
+		if (isFull())
+		{
+			System.out.println("OverFlow\nProgram Terminated\n");
+			System.exit(1);
+		}
+
+		System.out.println("Inserting " + x);
+		arr[++top] = x;
+	}
+
+	// Utility function to pop top element from the stack
+	public int pop()
+	{
+		// check for stack underflow
+		if (isEmpty())
+		{
+			System.out.println("UnderFlow\nProgram Terminated");
+			System.exit(1);
+		}
