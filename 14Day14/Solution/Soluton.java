801f5e8afd97629ec2106047837b5a847f1224c9
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 01:13:48 -0700

    Catch block for exception handling

--- 14Day14/Solution/Soluton.java
+++ 14Day14/Solution/Soluton.java
@@ -13,3 +13,11 @@ try{
     if(!name.find()){ // If string contains any number/symbols etc...
         throw new Exception("Not a perfect String");
     }
+
+
+        //This will not be executed if exception occurs
+    System.out.println("This will not be executed if exception occurs");
+
+}catch(Exception e){
+    System.out.println(e.toString());
+}
