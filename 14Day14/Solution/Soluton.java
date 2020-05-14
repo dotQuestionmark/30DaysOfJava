2dada68081491c4d5dd0a2c468774f244a802f80
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 01:11:02 -0700

    Input from user as user's name

--- 14Day14/Solution/Soluton.java
+++ 14Day14/Solution/Soluton.java
@@ -1,4 +1,9 @@
 import java.util.*;
 public class String{
 	public static void main(String[] args) {
+Scanner input =  new Scanner(System.in);
+String name = "";
+
+System.out.println("Please enter your name: ");
+name = input.nextLine();
 
