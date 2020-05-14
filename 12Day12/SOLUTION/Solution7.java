49f3ff480327c95974713f0868919038476991b3
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 00:47:10 -0700

    Function of stack implementation completed

--- 12Day12/SOLUTION/Solution7.java
+++ 12Day12/SOLUTION/Solution7.java
@@ -36,3 +36,37 @@ class Stack
 			System.out.println("UnderFlow\nProgram Terminated");
 			System.exit(1);
 		}
+		System.out.println("Removing " + peek());
+
+		// decrease stack size by 1 and (optionally) return the popped element
+		return arr[top--];
+	}
+
+	// Utility function to return top element in a stack
+	public int peek()
+	{
+		if (!isEmpty())
+			return arr[top];
+		else
+			System.exit(1);
+
+		return -1;
+	}
+
+	// Utility function to return the size of the stack
+	public int size()
+	{
+		return top + 1;
+	}
+
+	// Utility function to check if the stack is empty or not
+	public Boolean isEmpty()
+	{
+		return top == -1;	// or return size() == 0;
+	}
+
+	// Utility function to check if the stack is full or not
+	public Boolean isFull()
+	{
+		return top == capacity - 1;	// or return size() == capacity;
+	}
