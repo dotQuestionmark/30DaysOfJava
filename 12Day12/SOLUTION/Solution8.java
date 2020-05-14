4b07b8ef686826669b9d0dd427ef8c679deaf50c
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 00:50:47 -0700

    Class queue started

---
+++ 12Day12/SOLUTION/Solution8.java
@@ -0,0 +1,12 @@
+i/*
+ * Java Program to Implement Queue
+ */
+ 
+import java.util.*;
+ 
+/*  Class arrayQueue  */
+class arrayQueue
+{
+    protected int Queue[] ;
+    protected int front, rear, size, len;
+ 
