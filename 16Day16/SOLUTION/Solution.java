28bf4a5ed6aae336d41147666794bf1df8205ca9
Pranit5895 <guptapranit9@gmail.com>
2020-05-17 07:10:17 -0700

    CLass GFG started for function calling

--- 16Day16/SOLUTION/Solution.java
+++ 16Day16/SOLUTION/Solution.java
@@ -70,3 +70,16 @@ class Bike implements Vehicle {
     }
 
 }
+class GFG {
+
+    public static void main (String[] args) {
+
+        // creating an inatance of Bicycle
+        // doing some operations
+        Bicycle bicycle = new Bicycle();
+        bicycle.changeGear(2);
+        bicycle.speedUp(3);
+        bicycle.applyBrakes(1);
+
+        System.out.println("Bicycle present state :");
+        bicycle.printStates();
