b494c2acf2760ff6f3fa38ed93c12318d9a634da
Pranit5895 <guptapranit9@gmail.com>
2020-05-17 07:08:33 -0700

    Class Bicycle completed with functions implementing interface

--- 16Day16/SOLUTION/Solution.java
+++ 16Day16/SOLUTION/Solution.java
@@ -6,4 +6,35 @@ interface Vehicle {
     void changeGear(int a); 
     void speedUp(int a); 
     void applyBrakes(int a); 
-} 
+}
+class Bicycle implements Vehicle{
+
+    int speed;
+    int gear;
+
+     // to change gear
+    @Override
+    public void changeGear(int newGear){
+
+        gear = newGear;
+    }
+
+    // to increase speed
+    @Override
+    public void speedUp(int increment){
+
+        speed = speed + increment;
+    }
+
+    // to decrease speed
+    @Override
+    public void applyBrakes(int decrement){
+
+        speed = speed - decrement;
+    }
+
+    public void printStates() {
+         System.out.println("speed: " + speed
+              + " gear: " + gear);
+    }
+}
