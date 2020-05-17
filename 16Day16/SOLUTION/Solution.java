5d45169e7cf7700083a89f2134d45019afc9ef8b
Pranit5895 <guptapranit9@gmail.com>
2020-05-17 07:07:25 -0700

    Interface Vehicle declared

---
+++ 16Day16/SOLUTION/Solution.java
@@ -0,0 +1,9 @@
+import java.io.*; 
+  
+interface Vehicle { 
+      
+    // all are the abstract methods. 
+    void changeGear(int a); 
+    void speedUp(int a); 
+    void applyBrakes(int a); 
+} 
