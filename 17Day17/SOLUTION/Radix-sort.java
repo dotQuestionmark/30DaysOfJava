30ece60be171f6f1e7c85d95502f01f6c812ca62
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 23:48:37 -0700

    Output of array

--- 17Day17/SOLUTION/Radix-sort.java
+++ 17Day17/SOLUTION/Radix-sort.java
@@ -30,3 +30,15 @@ class Radix {
         // actual position of this digit in output[]
         for (i = 1; i < 10; i++)
             count[i] += count[i - 1];
+	 // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
