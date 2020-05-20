7460a9486369b53eea8da5f9eabc569f3911df55
Pranit5895 <guptapranit9@gmail.com>
2020-05-20 00:00:19 -0700

    Solution of day 18

---
+++ 18Day18/SOLFOLDER/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class test{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(system.in);
+		System.out.println("Enter courses: ");
+		int n;
+		n = scan.nextInt();
+		for ( int i=0; i<n; i++)
+		{
+			int a = scan.nextInt();
+		}
+		System.out.println("The courses you entered are", +a)
+	}
+
