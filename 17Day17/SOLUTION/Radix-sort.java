05fa1e69ba8290062a75ddc868894e88ab6fe0a5
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 23:49:25 -0700

    main radix sort function to sort the array

--- 17Day17/SOLUTION/Radix-sort.java
+++ 17Day17/SOLUTION/Radix-sort.java
@@ -42,3 +42,16 @@ class Radix {
         for (i = 0; i < n; i++)
             arr[i] = output[i];
     }
+    // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    static void radixsort(int arr[], int n)
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
