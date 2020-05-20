88321dcde53976836c3b8bddb7c54a002a2d6a50
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 23:45:13 -0700

    function to do counting sort of array

--- 17Day17/SOLUTION/Radix-sort.java
+++ 17Day17/SOLUTION/Radix-sort.java
@@ -12,4 +12,21 @@ class Radix {
             if (arr[i] > mx) 
                 mx = arr[i]; 
         return mx; 
-    } 
+    }
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
