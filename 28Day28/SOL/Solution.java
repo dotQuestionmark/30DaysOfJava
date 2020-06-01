108affbec0a27e80994d95be576202b108a56e9f
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:38:30 -0700

    Created Object of class,text field, buttons and some operators

--- 28Day28/SOL/Solution.java
+++ 28Day28/SOL/Solution.java
@@ -31,3 +31,14 @@ class calculator extends JFrame implements ActionListener {
         catch (Exception e) {
             System.err.println(e.getMessage());
         }
+	 // create a object of class
+        calculator c = new calculator();
+
+        // create a textfield
+        l = new JTextField(16);
+
+        // set the textfield to non editable
+        l.setEditable(false);
+
+        // create number buttons and some operators
+        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
