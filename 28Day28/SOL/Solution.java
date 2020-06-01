33377ad2be80ccb41bc4d7d3747d243a823cf980
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:39:15 -0700

    Created number buttons

--- 28Day28/SOL/Solution.java
+++ 28Day28/SOL/Solution.java
@@ -42,3 +42,14 @@ class calculator extends JFrame implements ActionListener {
 
         // create number buttons and some operators
         JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
+	// create number buttons
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
