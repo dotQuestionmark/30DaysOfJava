24ebd8375ec0890c4ac57d283fc5bf23c2f6d96f
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:34:56 -0700

    Class Calculator declared extending JFrame and implementing Action Listener

--- 28Day28/SOL/Solution.java
+++ 28Day28/SOL/Solution.java
@@ -3,4 +3,20 @@
   
 import java.awt.event.*; 
 import javax.swing.*; 
-import java.awt.*; 
+import java.awt.*;
+class calculator extends JFrame implements ActionListener {
+    // create a frame
+    static JFrame f;
+
+    // create a textfield
+    static JTextField l;
+
+    // store oprerator and operands
+    String s0, s1, s2;
+
+    // default constrcutor
+    calculator()
+    {
+        s0 = s1 = s2 = "";
+    }
+
