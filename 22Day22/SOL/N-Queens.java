339e57b569d2493497f2af03737abd3acf993b7f
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:06:20 -0700

    A utility function to print solution

--- 22Day22/SOL/N-Queens.java
+++ 22Day22/SOL/N-Queens.java
@@ -2,4 +2,13 @@
    backtracking */
 public class NQueenProblem { 
     final int N = 4; 
-  
+ /* A utility function to print solution */
+    void printSolution(int board[][]) 
+    { 
+        for (int i = 0; i < N; i++) { 
+            for (int j = 0; j < N; j++) 
+                System.out.print(" " + board[i][j] 
+                                 + " "); 
+            System.out.println(); 
+        } 
+    }  
