8f50df4b16b3491cf31b78f617b5d45ed0372afc
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:17:29 -0700

    Class Queen Declared

--- 24Day24/SOL/8-Queens.java
+++ 24Day24/SOL/8-Queens.java
@@ -2,3 +2,13 @@
 // backtracking.
 
 import java.util.*;
+public class Queens {
+    public static void main(String[] args) {
+        giveIntro();
+        Scanner console = new Scanner(System.in);
+        System.out.print("What size board do you want to use? ");
+        int size = console.nextInt();
+        System.out.println();
+        Board b = new Board(size);
+        solve(b);
+    }
