443b70e397e855d8ddf6bdd3ae58ce12a46989b1
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:07:21 -0700

    A utility function to check if a queen can be placed on board

--- 22Day22/SOL/N-Queens.java
+++ 22Day22/SOL/N-Queens.java
@@ -11,4 +11,31 @@ public class NQueenProblem {
                                  + " "); 
             System.out.println(); 
         } 
-    }  
+    }
+    /* A utility function to check if a queen can
+       be placed on board[row][col]. Note that this
+       function is called when "col" queens are already
+       placeed in columns from 0 to col -1. So we need
+       to check only left side for attacking queens */
+    boolean isSafe(int board[][], int row, int col)
+    {
+        int i, j;
+
+        /* Check this row on left side */
+        for (i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+
+        /* Check upper diagonal on left side */
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        /* Check lower diagonal on left side */
+        for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
