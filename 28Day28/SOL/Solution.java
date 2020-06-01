d3bf4309bb6a0a8b65f824eddaf3183edd9a5309
Pranit5895 <guptapranit9@gmail.com>
2020-06-01 00:41:05 -0700

    Added elements to panel

--- 28Day28/SOL/Solution.java
+++ 28Day28/SOL/Solution.java
@@ -87,3 +87,23 @@ class calculator extends JFrame implements ActionListener {
         be.addActionListener(c);
         beq.addActionListener(c);
         beq1.addActionListener(c);
+
+	// add elements to panel
+        p.add(l);
+        p.add(ba);
+        p.add(b1);
+        p.add(b2);
+        p.add(b3);
+        p.add(bs);
+        p.add(b4);
+        p.add(b5);
+        p.add(b6);
+        p.add(bm);
+        p.add(b7);
+        p.add(b8);
+        p.add(b9);
+        p.add(bd);
+        p.add(be);
+        p.add(b0);
+        p.add(beq);
+        p.add(beq1);
